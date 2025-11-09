91e42c7e8001512f640f8bcf4ed4e97442d452fa
fahimmuntacher <muntacherfahim234@gmail.com>
2025-11-09 15:36:23 +0600

    post and get method added

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+// transactions
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8xsgmgv.mongodb.net/?appName=Cluster0`;
 
@@ -26,6 +28,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const db = client.db("finmate");
+    const transactionsColl = db.collection("transactions");
+
+    // add transaction
+    app.post("/transactions", async (req, res) => {
+        const newTransaction = req.body;
+        const result = await transactionsColl.insertOne(newTransaction);
+        res.send(result);
+    })
+
+    // get transaction data
+    app.get("/transactions", async (req, res) => {
+        const cursor = transactionsColl.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
