2e21d03794f6cb18c1fd471825453108425432bb
fahimmuntacher <muntacherfahim234@gmail.com>
2025-11-10 15:59:06 +0600

    update transaction added

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-require("dotenv").config()
-var cors = require('cors')
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require("dotenv").config();
+var cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -12,7 +12,6 @@ app.use(express.json());
 
 // transactions
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8xsgmgv.mongodb.net/?appName=Cluster0`;
 
 const client = new MongoClient(uri, {
@@ -20,9 +19,8 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
-
 
 async function run() {
   try {
@@ -33,37 +31,64 @@ async function run() {
 
     // add transaction
     app.post("/transactions", async (req, res) => {
-        const newTransaction = req.body;
-        const result = await transactionsColl.insertOne(newTransaction);
-        res.send(result);
-    })
-
+      const newTransaction = req.body;
+      const result = await transactionsColl.insertOne(newTransaction);
+      res.send(result);
+    });
 
     // get transaction by email
-    
-    app.get("/transactions", async (req, res) => {
-      const email = req.query.email;
 
-      const query = email ? { userEmail: email } : {};
-      const cursor = transactionsColl.find(query);
+    app.get("/transactions", async (req, res) => {
+      const { email, type, sortByDate } = req.query;
+      const query = {};
+      if (email) query.userEmail = email;
+      if (type) query.type = type;
+
+      let cursor = transactionsColl.find(query);
+      if (sortByDate) {
+        cursor = cursor.sort({ date: sortByDate === "asc" ? 1 : -1 });
+      }
       const result = await cursor.toArray();
       res.send(result);
     });
 
     // get details
 
-    app.get("/transactions/:id", async(req, res) => {
+    app.get("/transactions/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
+      const query = { _id: new ObjectId(id) };
       const cursor = await transactionsColl.findOne(query);
-      
+
+      res.send(cursor);
+    });
+
+    // update transaction 
+
+    app.put("/transactions/:id", async(req, res) => {
+      const id = req.params.id;
+      const updatedTrans = req.body
+      const query = {_id : new ObjectId(id)};
+      const update = {
+         $set : updatedTrans
+      }
+      const option = {};
+      const result = await transactionsColl.updateOne(query, update, option);
+      res.send(result);
+    })
+
+    // delete transaction
+    app.delete("/transactions/:id", async(req, res) => {
+      const id = req.params.id;
+      const query = {_id : new ObjectId(id)};
+      const cursor = await transactionsColl.deleteOne(query);
       res.send(cursor)
     })
 
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -72,11 +97,10 @@ async function run() {
 
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
